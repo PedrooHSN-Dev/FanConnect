3e236e37a9153c59ab26ff8359891527ce8a753b
Pedro Henrique Silva <pppedrohenri@gmail.com>
2026-06-03 15:17:48 -0300

    feat: implementa endpoints para agenda e cadastro de usuarios
    - Criacao do ItemAgendaRepository para persistencia de eventos
    - Construcao do AgendaController com rotas GET e POST para gerenciar a agenda academica
    - Adicao do UsuarioController com rotas GET e POST permitindo o cadastro via API REST

--- src/main/java/br/com/fanconnect/controller/AgendaController.java
+++ src/main/java/br/com/fanconnect/controller/AgendaController.java
@@ -1,4 +1,34 @@
 package br.com.fanconnect.controller;
 
+import br.com.fanconnect.entity.ItemAgenda;
+import br.com.fanconnect.repository.ItemAgendaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/agenda")
 public class AgendaController {
+
+    private final ItemAgendaRepository agendaRepository;
+
+    public AgendaController(ItemAgendaRepository agendaRepository) {
+        this.agendaRepository = agendaRepository;
+    }
+
+    // 1. Listar todos os eventos (GET)
+    @GetMapping
+    public ResponseEntity<List<ItemAgenda>> listarEventos() {
+        List<ItemAgenda> eventos = agendaRepository.findAll();
+        return ResponseEntity.ok(eventos);
+    }
+
+    // 2. Criar um novo evento (POST)
+    @PostMapping
+    public ResponseEntity<ItemAgenda> criarEvento(@RequestBody ItemAgenda novoEvento) {
+        ItemAgenda eventoSalvo = agendaRepository.save(novoEvento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(eventoSalvo);
+    }
 }
